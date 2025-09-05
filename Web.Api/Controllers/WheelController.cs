d9513dd2cf516a7f115bd8a0c1158d8ec015205c
TheHarmont <Golikovvladislav2000@gmail.com>
2025-09-05 22:33:42 +1000

    add WheelController, but with test actions

---
+++ Web.Api/Controllers/WheelController.cs
@@ -0,0 +1,31 @@
+﻿using Domain.Primitives;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Web.Api.Extensions;
+using Web.Api.Tools;
+
+namespace Web.Api.Controllers;
+
+[Route("api/[controller]")]
+[Authorize]
+[ApiController]
+public class WheelController : BaseController
+{
+    [HttpGet("GetPrizes")]
+    public async Task<IResult> GetPrizes(CancellationToken ct = default)
+    {
+        await Task.Delay(1000);
+        string[] prizes = new[] { "Приз1", "Приз2", "Приз3", "Приз4", "Приз5", "Приз6", "Приз7", };
+        var result = Result.Success(prizes); 
+        return result.Match(Results.Ok, CustomResults.Problem);
+    }
+
+    [HttpGet("GetPrize")]
+    public async Task<IResult> GetPrize(CancellationToken ct = default)
+    {
+        await Task.Delay(1000);
+        string[] prizes = new[] { "Приз1", "Приз2", "Приз3", "Приз4", "Приз5", "Приз6", "Приз7", };
+        var result = Result.Success(prizes[new Random().Next(prizes.Length-1)]);
+        return result.Match(Results.Ok, CustomResults.Problem);
+    }
+}
