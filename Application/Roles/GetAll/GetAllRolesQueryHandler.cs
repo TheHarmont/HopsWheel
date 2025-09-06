9eff9c4dc48d924241f7706cb9101090c66f0089
TheHarmont <Golikovvladislav2000@gmail.com>
2025-09-06 15:15:30 +1000

    Add GetAllRolesQuery

---
+++ Application/Roles/GetAll/GetAllRolesQueryHandler.cs
@@ -0,0 +1,17 @@
+﻿using Application.Abstractions;
+using Application.Users.GetAll;
+using Domain.Entities;
+using Domain.Primitives;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Roles.GetAll;
+internal sealed class GetAllRolesQueryHandler() : IRequestHandler<GetAllRolesQuery, Result<string[]>>
+{
+    public async Task<Result<string[]>> Handle(GetAllRolesQuery query, CancellationToken ct = default)
+    {
+        string[] roles = System.Enum.GetNames(typeof(Role));
+
+        return Result.Success(roles);
+    }
+}
