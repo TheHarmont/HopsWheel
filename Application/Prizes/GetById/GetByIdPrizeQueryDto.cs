2b50ebef1c08fc075a9b274ecdb6483b3b2bec25
TheHarmont <Golikovvladislav2000@gmail.com>
2025-09-06 15:15:15 +1000

    Add GetByIdPrizeQuery

---
+++ Application/Prizes/GetById/GetByIdPrizeQueryDto.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Application.Prizes.GetById;
+public class GetByIdPrizeQueryDto
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public int Weight { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsDeleted { get; set; }
+    public int MaxUses { get; set; }
+}
---
+++ Application/Prizes/GetById/GetByIdPrizeQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using Application.Abstractions;
+using Domain.Primitives;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Prizes.GetById;
+internal sealed class GetByIdPrizeQueryHandler(
+    IPrizeRepository prizeRepository) : IRequestHandler<GetByIdPrizeQuery, Result<GetByIdPrizeQueryDto>>
+{
+    public async Task<Result<GetByIdPrizeQueryDto>> Handle(GetByIdPrizeQuery query, CancellationToken ct = default)
+    {
+        var prize = await prizeRepository.GetAsync(u => u.Id == query.Id);
+
+        if (prize == null)
+        {
+            return Result.Failure<GetByIdPrizeQueryDto>(Error.NotFound("Prize.NotFound", "Объект не найден"));
+        }
+
+        var response = new GetByIdPrizeQueryDto
+        {
+            Id = prize.Id,
+            Name = prize.Name,
+            Weight = prize.Weight,
+            MaxUses = prize.MaxUses,
+            IsActive = prize.IsActive,
+            IsDeleted = prize.IsDeleted
+        };
+
+        return Result.Success(response);
+    }
+}
