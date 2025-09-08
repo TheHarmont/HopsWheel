e34e9f5c46972710b087374edad0aca63f13ddd7
TheHarmont <Golikovvladislav2000@gmail.com>
2025-09-08 21:53:39 +1000

    add method in PrizeRepository

--- Application/Abstractions/IPrizeRepository.cs
+++ Application/Abstractions/IPrizeRepository.cs
@@ -5,4 +5,5 @@ namespace Application.Abstractions;
 
 public interface IPrizeRepository : IAsyncRepository<Prize>
 {
+    public Task<List<Prize>> GetAvailablePrizes(CancellationToken ct = default);
 }
--- Infrastructure/Repositories/PrizeRepository.cs
+++ Infrastructure/Repositories/PrizeRepository.cs
@@ -2,8 +2,18 @@
 using Core.Infrastructure.Repositories;
 using Domain.Entities;
 using Infrastructure.Context;
+using Microsoft.EntityFrameworkCore;
+using System.Collections;
 
 namespace Infrastructure.Repositories;
 public class PrizeRepository(ApplicationDbContext context) : RepositoryBase<Prize, ApplicationDbContext>(context), IPrizeRepository
 {
+    public async Task<List<Prize>> GetAvailablePrizes(CancellationToken ct = default)
+    {
+        return await context
+            .Set<Prize>()
+            .AsNoTracking()
+            .Where(prize => !prize.IsDeleted && prize.IsActive)
+            .ToListAsync(ct) ?? new();
+    }
 }
